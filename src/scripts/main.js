90119b73a45217cce2b02ced111504a24d0ed460
Tausvokse <parhom137@gmail.com>
2025-10-01 18:08:57 +0300

    Added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+let totalPopulation = 0;
+
+document.querySelectorAll('.population').forEach((el) => {
+  totalPopulation += parseInt(el.innerText.replace(/,/g, ''), 10);
+});
+
+document.querySelector('.total-population').innerText =
+  totalPopulation.toLocaleString('en-US');
+
+document.querySelector('.average-population').innerText = (
+  totalPopulation / document.querySelectorAll('.population').length
+).toLocaleString('en-US');
